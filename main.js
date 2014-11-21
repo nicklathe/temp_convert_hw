3374192ff9df2f52fa7b8ec9718fe8376899dd13
Nick Lathe <nicklathe@icloud.com>
2014-11-21 12:29:34 -0800

    Few more tweaks with the look

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+//waits for document to load
 document.addEventListener("DOMContentLoaded", function() {
+	//listens for button click
 	document.querySelector(".btn-default").addEventListener("click", function() {
+		//gets value from input box, then performs function
 		var tempF = parseInt(document.querySelector(".form-control").value);
 		var tempC = ((tempF - 32) * (5 / 9)).toFixed(0);
+		//checks to see if it's a number, then depending on what number, will change the div color
 			if(isNaN(tempF)) {
 			document.querySelector("h4").innerText = "That is not a temperature, idiot! Enter a number.";
 		    } else if(tempC > 0) {
@@ -11,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		    	document.querySelector(".top-color").style.backgroundColor = "#9D9AF5";
 		    	document.querySelector("h4").innerText = "The Temperature in Celsius is: " + tempC + " C";
 		    }
-	});
+	}); //function to reload page 
 	document.querySelector(".btn-primary").addEventListener("click", function() {
 		window.location.reload();
 	});
