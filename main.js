db429eab5f62f44dcf51c9901f1ed377f193cb98
Nick Lathe <nicklathe@icloud.com>
2014-11-21 09:28:20 -0800

    Added a feature that updates the color of the h4 tag

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ document.addEventListener("DOMContentLoaded", function() {
 		var tempC = ((tempF - 32) * (5 / 9)).toFixed(0);
 			if(isNaN(tempF)) {
 			document.querySelector("h4").innerText = "That is not a temperature, idiot! Enter a number.";
+		    } else if(tempC > 0) {
+		    	document.querySelector("h4").style.color = "red";
+		    	document.querySelector("h4").innerText = "The Temperature in Celsius is: " + tempC + " C";
 		    } else {
-			document.querySelector("h4").innerText = "The Temperature in Celsius is: " + tempC + " C";
-		}
+		    	document.querySelector("h4").style.color = "blue";
+		    	document.querySelector("h4").innerText = "The Temperature in Celsius is: " + tempC + " C";
+		    }
 	});
 	document.querySelector(".btn-primary").addEventListener("click", function() {
 		window.location.reload();
 	});
 });
-
-
